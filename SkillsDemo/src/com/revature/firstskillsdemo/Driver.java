94054c8c723fa1bdfa99c2b07aae32978275f536
Melissa Yates <melissampeart@gmail.com>
2020-04-20 16:46:54 -0500

    making progress- third commit

--- SkillsDemo/src/com/revature/firstskillsdemo/Driver.java
+++ SkillsDemo/src/com/revature/firstskillsdemo/Driver.java
@@ -1,9 +1,19 @@
 package com.revature.firstskillsdemo;
 
+import java.util.Scanner;
+
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter the name of a game: ");
+		String nameOfGame = scan.nextLine();
+		
+		System.out.print("Enter your score: ");
+		String playerScore = scan.nextLine();
+		Integer score = Integer.parseInt(playerScore);
+		
+		
 
 	}
 
